bf9d5fc4b21e0da1f172b098f5bf43228719592d
Unai Compaired <88508192+unaiccz@users.noreply.github.com>
2025-05-08 11:40:32 +0200

    shore(browserrouter to hashrouter)

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,11 @@
-// src/main.jsx
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import ROUTER from "./router/router.jsx";
+import "./index.css";
 
-import NotFound from './pages/NotFound'
-import Perfil from './pages/Perfil'
-import Mapa from './pages/Mapa'
-import Login from './pages/Login'
-import MostrarIncidencia from './pages/MostrarIncidencia'
-import Estadisticas from './pages/Estadisticas'
-import CrearIncidencia from './pages/CrearIncidencia'
-import Personas from './pages/Personas'
-import PaginaPrincipal from './pages/PaginaPrincipal'
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<PaginaPrincipal />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/perfil" element={<Perfil />} />
-        <Route path="/mapa" element={<Mapa />} />
-        <Route path="/incidencia" element={<MostrarIncidencia />} />
-        <Route path="/estadisticas" element={<Estadisticas />} />
-        <Route path="/crear-incidencia" element={<CrearIncidencia />} />
-        <Route path="/personas" element={<Personas />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterProvider router={ROUTER} />
   </StrictMode>
-)
+);
