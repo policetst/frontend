92373863024827842d911d8e9c6f78e6ac5f2e91
Unai Compaired <88508192+unaiccz@users.noreply.github.com>
2025-05-14 20:44:30 +0200

    fix(vite config): add some configurations for enable async await in modern browsers

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: 'esnext', // Cambia esto a 'esnext' para usar las últimas características de JavaScript
+  },
   plugins: [
     react(),
     tailwindcss() // agregar el plugin de Tailwind CSS
