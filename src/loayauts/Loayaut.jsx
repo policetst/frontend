034bb592402d1e6f6c1fe092a1dfe5414dba74d1
sbeamud <sergiobeamud@iesrioarba.es>
2025-05-14 12:02:43 +0200

    Update Loayaut.jsx

--- src/loayauts/Loayaut.jsx
+++ src/loayauts/Loayaut.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link, Outlet } from 'react-router-dom';
-import { Menu, User, LogOut, Home, Settings } from 'lucide-react';
 import '../index.css';
 import { Menu, X, User, LogOut, Home, Settings } from 'lucide-react';
 
